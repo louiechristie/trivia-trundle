296cfb651ce06f5aeca2b7c40b25d0f2ef573c05
Your Name <you@example.com>
2022-05-03 20:29:38 +0100

    feat: make questions friendlier

--- api/questionsAPI.ts
+++ api/questionsAPI.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean',
+  baseURL: 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean&category=9',
 });
