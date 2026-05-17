8f11520511b7376966c9ac6d928fa1afe4f6d5d5
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2026-05-17 11:10:07 +0100

    fix: header background color

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,12 +3,14 @@ import { useRouter } from 'expo-router';
 import * as React from 'react';
 import { Appbar } from 'react-native-paper';
 
+import Colors from '../constants/Colors';
+
 const Header = (): React.JSX.Element => {
   const description = Constants.expoConfig?.description;
   const name = Constants.expoConfig?.name;
   const router = useRouter();
   return (
-    <Appbar.Header dark>
+    <Appbar.Header dark style={{ backgroundColor: Colors.light.colors.primary }}>
       <Appbar.Action icon="home" onPress={() => router.push('/')} />
       <Appbar.Content title={name} subtitle={description} />
     </Appbar.Header>
